2472c7180f4ad31b754dc7063ddad1047a1c4353
Gabe Mulley <gabe@edx.org>
2015-03-27 23:29:18 -0400

    another fix

--- edx/analytics/monitor/hadoopjmx.py
+++ edx/analytics/monitor/hadoopjmx.py
@@ -1,6 +1,9 @@
 
+import re
 import requests
 
+
+#"name" : "Hadoop:service=JobTracker,name=jvm"
 
 class HadoopJmx(object):
 
@@ -20,24 +23,6 @@ class HadoopJmx(object):
                     committed = heap_stats.get('committed')
                     maximum = heap_stats.get('max')
                     self.heap_used_percent = (float(committed) / maximum) * 100
-            elif name == 'java.lang:type=GarbageCollector,name=MarkSweepCompact':
-                self.extract_gc_stats(bean, 'mark_sweep')
-            elif name == 'java.lang:type=GarbageCollector,name=Copy':
-                self.extract_gc_stats(bean, 'copy')
-
-    def extract_gc_stats(self, bean, prefix):
-        count = bean.get('CollectionCount')
-        if count:
-            setattr(self, 'gc_{0}_count'.format(prefix), count)
-
-        time = bean.get('CollectionTime')
-        if time:
-            setattr(self, 'gc_{0}_time'.format(prefix), time)
-
-    @property
-    def gc_count(self):
-        return self.gc_mark_sweep_count + self.gc_copy_count
-
-    @property
-    def gc_time(self):
-        return self.gc_mark_sweep_time + self.gc_copy_time
+            elif re.match(r'Hadoop:service=\w+,name=jvm', name):
+                self.gc_count = bean.get('gcCount', -1)
+                self.gc_time = bean.get('gcTimeMillis', 0)
